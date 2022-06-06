9826b1280785a3878d4a38d1f03b903ea7107e8a
matheuspm97 <matheus_pm97@yahoo.com.br>
2022-06-06 17:51:08 +0000

    Atividade 2 Heart desease

--- app.py
+++ app.py
@@ -12,17 +12,24 @@ def index():
 
 @app.route('/predicao', methods=['POST'])
 def predicao():
-  sex = request.form['sex']
-  age = request.form['age']
-  cp = request.form['cp']
-  chol = request.form['chol']
-  thal = request.form['thal']
-  thalach = request.form['thalach']
-  exang = request.form['exang']
-  predicao = model.predict([sex, age, cp, chol, thal, thalach, exang])
+  sex = int(request.form['sex'])
+  age = int(request.form['age'])
+  cp = int(request.form['cp'])
+  chol = int(request.form['chol'])
+  thal = int(request.form['thal'])
+  thalach = int(request.form['thalach'])
+  exang = int(request.form['exang'])
+  predicao = model.predict([[sex, age, cp, chol, thal, thalach, exang]])
   return render_template('resposta.html', predicao=predicao[0])
 
 app.run(debug=True)
 
 # pip install -r requirements.txt (instala as bibliotecas)
 # python app.py (para executar)
+
+# git add .
+# git commit -m "nomenovo"
+# git push
+
+#0 - Baixa probabilidade de ter doença cardiovascular
+#1 - Alta probabilidade de ter doença cardiovasculas
